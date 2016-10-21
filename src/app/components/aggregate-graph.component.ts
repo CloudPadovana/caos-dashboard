048bc13b6b26ba529f67c12160da8cfacbf5ace6
Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>
2016-10-21 15:18:52 +0200

    Add "Download data" button to download CSV data
    Signed-off-by: Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>

--- src/app/components/aggregate-graph.component.ts
+++ src/app/components/aggregate-graph.component.ts
@@ -131,6 +131,70 @@ export class AggregateGraphComponent implements OnInit, AfterViewInit, OnChanges
     return this._dataseries_sum[p.id];
   };
 
+  private data_to_CSV(): string {
+    let s: string[] = [];
+
+    let row: string[] = [];
+    row.push("From");
+    row.push("To");
+    this.projects.map((p: Project) => row.push(p.name));
+    s.push(row.join());
+
+    let ts: Date[] = [];
+
+    for (let series of this.data) {
+      series.values.map((a: Aggregate) => {
+        if (!ts.find((d: Date) => moment(d).isSame(a.timestamp))) {
+          ts.push(new Date(a.timestamp));
+        }
+      });
+    }
+
+    let sorted_ts = ts.sort(
+      (d1: Date, d2: Date) => ( d1.valueOf() - d2.valueOf() ));
+
+    for (let t of sorted_ts) {
+      row = [];
+      row.push(moment(t).subtract(this.granularity, 'second').toString());
+      row.push(moment(t).toString());
+
+      for (let p of this.projects) {
+        let tmp = this.dataseries(p).values.find((a: Aggregate) => moment(t).isSame(a.timestamp));
+        if (tmp) {
+          row.push((tmp.sum/3600).toString());
+        } else {
+          row.push("NaN");
+        }
+      }
+      s.push(row.join());
+    }
+
+    row = [];
+    row.push(moment(this.daterange.start).toString());
+    row.push(moment(this.daterange.end).toString());
+    for (let p of this.projects) {
+      row.push((this.dataseries_sum(p)/3600).toString());
+    }
+    s.push(row.join());
+    return s.join('\n');
+  }
+
+  download_CSV(filename: string) {
+    let data = this.data_to_CSV();
+
+    let blob = new Blob([data], { type: 'text/csv' });
+    let url = window.URL.createObjectURL(blob);
+
+    let a = document.createElement("a");
+    a.setAttribute('style', 'display:none;');
+    document.body.appendChild(a);
+
+    a.href = url;
+    a.download = filename;
+
+    a.click();
+  }
+
   ngAfterViewInit() {
     this.update_chart();
   }
